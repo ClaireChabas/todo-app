8bfe876c9f2481b97cc8747d704b536b2cde6236
EC2 Default User <ec2-user@ip-172-31-11-21.us-east-2.compute.internal>
2018-05-03 14:12:58 +0000

    Implementing the POST route to create a new to-do in db and return its data as the response to display it later on the screen

--- todo_api/index.js
+++ todo_api/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var bodyParser = require('body-parser');
 
 // Importing all our routes from routes/todos.js
 var todoRoutes = require('./routes/todos');
+
+// We use body-parser to access the request body from a POST or PUT request
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
     res.send('Hello I am the root route');
