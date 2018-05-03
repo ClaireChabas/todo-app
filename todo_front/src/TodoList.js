6d5460d0f31f4fdbbed369d30bd6b6ba2e8878e1
EC2 Default User <ec2-user@ip-172-31-11-21.us-east-2.compute.internal>
2018-05-03 15:49:59 +0000

    Creating top level component TodoList which will hold the state for the to-do app

---
+++ todo_front/src/TodoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+    render() {
+        return (
+            <h1>To-do List</h1>
+        )
+    }
+}
+
+export default TodoList;
