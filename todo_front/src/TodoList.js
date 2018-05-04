3ff6d2ea2a8b7a3aa313a6e03d00960055ce02de
EC2 Default User <ec2-user@ip-172-31-11-21.us-east-2.compute.internal>
2018-05-04 09:46:32 +0000

    Implementing the fetch of our to-do json api data

--- todo_front/src/TodoList.js
+++ todo_front/src/TodoList.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
+const API_URL = '/api/todos';
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        };
+    }
+    
+    componentWillMount() {
+        this.loadTodos();
+    }
+    
+    loadTodos() {
+        fetch(API_URL)
+        .then(resp => {
+            if(!resp.ok) {
+                if(resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    });
+                } else {
+                    let err = {errorMessage: 'Server is not responding. Please try again later.'};
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+        .then(todos => this.setState({todos}));
+    }
+    
     render() {
         return (
             <h1>To-do List</h1>
